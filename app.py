24ab56e4cc3f5697f6c194d3de541f9fdc03095b
ratroot92 <zcomisb.007@gmail.com>
2023-03-15 21:27:34 +0500

    ...

--- app.py
+++ app.py
@@ -22,24 +22,48 @@ from utils.util import Utils
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from jwt.exceptions import ExpiredSignatureError
+import os
 
 
-# def do_something():
-#     print('MyFlaskApp is starting up!')
+def worker():
+    try:
+        # snsScrapper = sntwitter.TwitterSearchScraper('fire')
+        # for counter, tweet in enumerate(snsScrapper.get_items()):
+        #     if counter > 10:
+        #         break
+        #     print(tweet.rawContent)
+        targets = db.targets.find({})
+        for target in targets:
+            # if target['status'] == 0:
+            target['_id'] = str(target['_id'])
+            Scrapper.scrapKeywords(target)
+            print("Process Complete!!! for "+target['_id']+" " + target['targetType'])
+    except Exception as e:
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        print(e)
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
 
-# class MyFlaskApp(Flask):
-#     def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
-#         if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
-#             with self.app_context():
-#                 do_something()
-#         super(MyFlaskApp, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
+def activateTaskScheduler():
+    scheduler = BackgroundScheduler(daemon=True)
+    scheduler.add_job(func=worker, trigger='interval', seconds=30)
+    scheduler.start()
+    print(" >>> Scheduler started")
 
 
-app = Flask(__name__)
+class MyFlaskApp(Flask):
+    def run(self, host=None, port=5000, debug=True, load_dotenv=True, **options):
+        if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
+            with self.app_context():
+                activateTaskScheduler()
+        super(MyFlaskApp, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
+
+
+app = MyFlaskApp(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = '004f2af45d3a4e161a7dd2d17fdae47f'
-
+# if __name__ == '__main__':
+app.run()
 
 # def auth_middleware(app):
 #     @app.before_request
@@ -127,6 +151,7 @@ def userLogin():
             context = {'user': user}
             return render_template('dashboard.html', context=context)
         else:
+            flash('Invalid Credentials.')
             return redirect(url_for('getLoginPage'))
     else:
         return redirect(url_for('getLoginPage'))
@@ -139,256 +164,24 @@ def addUserTarget():
         targets = request.form['targets']  # type: ignore
         limit = request.form['limit']  # type: ignore
         userId = ObjectId(session.get('userId'))  # type: ignore
-        user = db.users.find_one({'_id': userId})
-        if targetType and targets and limit and user:
-            targetExist = db.targets.find_one({'targetType': targetType, 'user': user['_id']})
-            if not targetExist:
-                target = Target(targetType=targetType, targets=targets, limit=limit, user=user['_id'])
-                target = db.targets.insert_one(target.toDictionary())
-                flash('Target created successfully!')
-                return redirect('/dashboard')
+        targets = targets.split(',')
+        if (len(targets)):
+            for target in targets:
+                target = target.strip()
+            user = db.users.find_one({'_id': userId})
+            if targetType and targets and limit and user:
+                targetExist = db.targets.find_one({'targetType': targetType, 'user': user['_id']})
+                if not targetExist:
+                    target = Target(targetType=targetType, targets=targets, limit=int(limit), user=user['_id'])
+                    target = db.targets.insert_one(target.toDictionary())
+                    flash('Target created successfully!')
+                    return redirect('/dashboard')
+                else:
+                    flash('Target already exists')
+                    return redirect('/dashboard')
             else:
-                flash('Target already exists')
+                flash('Something went wrong.')
                 return redirect('/dashboard')
-        else:
-            flash('Something went wrong.')
-            return redirect('/dashboard')
     except Exception as e:
         flash(str(e))
         return redirect('/dashboard')
-
-
-# @app.route('/login', methods=['POST'])
-# def login():
-#     error = None
-#     if request.method == 'POST':
-#         try:
-#             reqBody = request.get_json()
-#             if 'email' not in reqBody:
-#                 return Utils.BadRequestResponse('email is required.')
-#             if 'password' not in reqBody:
-#                 return Utils.BadRequestResponse('password is required.')
-
-#             user = User.UserExists(
-#                 {'email': reqBody['email'], 'password': reqBody['password']})
-#             if not user:
-#                 error = "Invalid credentials. Please try again."
-#                 return Utils.UnauthorizedResponse('Invalid credentials.')
-#             else:
-#                 data = {"_id": str(user["_id"]), "email": user["username"], "firstName": user["firstName"],
-#                         "lastName": user["lastName"], "email": user["email"]}
-#                 token = jwt.encode({'_id': data["_id"], 'exp': datetime.datetime.utcnow(
-#                 ) + datetime.timedelta(minutes=45)}, app.config['SECRET_KEY'], "HS256")
-#                 response = make_response(jsonify(
-#                     {"data": data, "message": "Logged in successfully", "success": True, "token": token}), 200)
-#                 session['is_authenticated'] = True
-#                 print(">>>>>>>>>>>>>>>>>>>>>>>>")
-#                 print(">>>>>>>>>>>>>>>>>>>>>>>>")
-#                 print(">>>>>>>>>>>>>>>>>>>>>>>>")
-#                 print(">>>>>>>>>>>>>>>>>>>>>>>>")
-
-#                 return render_template('dashboard.html')
-#         except Exception as e:
-#             return Utils.ErrorResponse('Someting went wrong.')
-#     return render_template("login.html", error=error)
-
-
-# @app.route('/user', methods=['DELETE'])
-# def delete_user(authUser):
-#     try:
-#         reqBody = request.get_json()
-#         if '_id' not in reqBody:
-#             return Utils.BadRequestResponse('_id is required.')
-#         user = db.users.find_one({'_id': ObjectId(reqBody['_id'])})
-#         if user:
-#             db.targets.delete_many({'user': ObjectId(reqBody['_id'])})
-#             result = db.users.delete_one({'_id': ObjectId(authUser["_id"])})
-#             if result.deleted_count == 1:
-#                 return Utils.SuccessResponse({}, "User deleted successfully")
-#             else:
-#                 return Utils.NotFoundResponse(reqBody, 'Error deleting user')
-#         else:
-#             return "User not FOund"
-
-#     except Exception as e:
-#         return Utils.ErrorResponse('Someting went wrong.')
-
-
-# @app.route('/user/seed', methods=['GET'])
-# def seed():
-#     try:
-#         data = User.seed()
-#         return Utils.SuccessResponse(data, "User seed successfully")
-#     except Exception as e:
-#         return Utils.ErrorResponse('Someting went wrong.')
-# # Define some heavy function
-
-
-# @app.route('/user/target/keywords', methods=['POST'])
-# def setUserTargets(authUser):
-#     try:
-#         reqBody = request.get_json()
-#         if 'targetType' not in reqBody:
-#             return Utils.BadRequestResponse('targetType is required.')
-
-#         if reqBody['targetType'] != 'keywords' and reqBody['targetType'] != 'twitter-hashtag' and reqBody['targetType'] != 'twitter-user':
-#             return Utils.BadRequestResponse('Invalid "targetType" .')
-
-#         if 'targets' not in reqBody and len(reqBody['targets']) == 0:
-#             return Utils.BadRequestResponse('targets is required.')
-
-#         if 'limit' not in reqBody:
-#             return Utils.BadRequestResponse('limit is required.')
-
-#         if reqBody['limit'] > 1000:
-#             return Utils.BadRequestResponse('maximum limit is 1000.')
-
-#         exist = Target.TargetExist(reqBody)
-#         if not exist:
-#             target = Target(
-#                 targetType=reqBody["targetType"], targets=reqBody["targets"], limit=reqBody['limit'], user=authUser['_id'])
-#             target = db.targets.insert_one(target.toDictionary())
-#             target = db.targets.find_one({'_id': target.inserted_id})
-#             target['_id'] = str(target['_id'])
-#             target['user'] = str(target['user'])
-#             heavyTask = Process(target=scrapLater, args=(target,))
-#             heavyTask.start()
-#             return Utils.SuccessResponse(target, "Target created successfully")
-#         else:
-#             response = Utils.NotFoundResponse(
-#                 exist, "Target Type '" + reqBody["targetType"]+"' already exists.")
-#             return response
-
-#     except Exception as e:
-#         return Utils.ErrorResponse('Someting went wrong.')
-
-
-# @app.route('/user/target/keywords', methods=['GET'])
-# def getUserTargets():
-#     try:
-#         # data = Target.GetUserTargets(authUser)
-#         # return Utils.SuccessResponse(data, "All user targets.")
-#         return render_template("dashboard.html")
-#     except Exception as e:
-#         return Utils.ErrorResponse('Someting went wrong.')
-
-
-# @app.route('/user/target/keywords', methods=['PUT'])
-# def updateUserTargets(authUser):
-#     try:
-#         reqBody = request.get_json()
-#         if 'targetType' not in reqBody:
-#             return Utils.BadRequestResponse('targetType is required.')
-
-#         if reqBody['targetType'] != 'keywords' and reqBody['targetType'] != 'twitter-hashtag' and reqBody['targetType'] != 'twitter-user':
-#             return Utils.BadRequestResponse('Invalid "targetType" .')
-
-#         if 'targets' not in reqBody and len(reqBody['targets']) == 0:
-#             return Utils.BadRequestResponse('targets is required.')
-
-#         if 'limit' not in reqBody:
-#             return Utils.BadRequestResponse('limit is required.')
-
-#         if reqBody['limit'] > 1000:
-#             return Utils.BadRequestResponse('maximum limit is 1000.')
-#         exist = Target.TargetExist(reqBody)
-#         if exist:
-#             target = db.targets.update_one({'_id': ObjectId(exist['_id'])}, {"$set": {
-#                                            'targets': reqBody['targets'], 'tweets': [], 'limit': reqBody['limit'], 'status': 0}})
-#             if target.matched_count > 0 and target.modified_count > 0:
-#                 target = db.targets.find_one({'_id': ObjectId(exist['_id'])})
-#                 target['_id'] = str(target['_id'])
-#                 target['user'] = str(target['user'])
-#                 return Utils.SuccessResponse(target, "Target updated successfully")
-#             else:
-#                 return Utils.NotFoundResponse(exist, "Failed to update target.")
-#         else:
-#             return Utils.NotFoundResponse({}, "Target not found.")
-#     except Exception as e:
-#         return Utils.ErrorResponse('Someting went wrong.')
-
-
-# @app.route('/user/target/keywords', methods=['DELETE'])
-# def deleteUserTargets(authUser):
-#     try:
-#         reqBody = request.get_json()
-#         if 'targetType' not in reqBody:
-#             return Utils.BadRequestResponse('targetType is required.')
-#         else:
-#             userTarget = db.targets.find_one(
-#                 {'user': authUser['_id'], 'targetType': reqBody['targetType']})
-#             if userTarget:
-#                 result = db.targets.delete_one(
-#                     {'user': authUser['_id'], 'targetType': reqBody['targetType']})
-#                 if result.deleted_count == 1:
-#                     return Utils.SuccessResponse(reqBody, 'Target deleted successfully.')
-#                 else:
-#                     return Utils.NotFoundResponse('Failed to delete '+reqBody['targetType']+" target.")
-#             else:
-#                 return Utils.NotFoundResponse("Target not found")
-#     except Exception as e:
-#         return Utils.ErrorResponse('Someting went wrong.')
-
-
-# @app.route('/user', methods=['POST'])
-# def createUser(authUser):
-#     try:
-#         reqBody = request.get_json()
-#         if reqBody is None:
-#             return jsonify({"success": False, 'message': 'Invalid JSON'}), 400
-#         user = User(
-#             firstName=reqBody["firstName"],
-#             lastName=reqBody["lastName"],
-#             username=reqBody["username"],
-#             password=reqBody["password"],
-#             email=reqBody["email"]
-#         )
-#         exists = User.UserExists(
-#             {'username': reqBody['username'], 'email': reqBody['email']})
-#         if not exists:
-#             user = db.users.insert_one(user.toDictionary())
-#             user = db.users.find_one({'_id': user.inserted_id})
-#             user['_id'] = str(user['_id'])
-#             # data = {"_id": str(user["_id"]), "username": user["username"], "firstName": user["firstName"], "lastName": user["lastName"], "email": user["email"]}
-#             return Utils.SuccessResponse(user, "User created successfully")
-#         else:
-#             return Utils.NotFoundResponse(exists, "User already exists")
-#     except Exception as e:
-#         return Utils.ErrorResponse('Someting went wrong.')
-
-# @app.route('/user', methods=['GET'])
-# def getAllUsers(authUser):
-#     try:
-#         data = []
-#         users = db.users.find()
-#         for user in users:
-#             data.append({"_id": str(user["_id"]), "username": user["username"],
-#                         "firstName": user["firstName"], "lastName": user["lastName"], "email": user["email"]})
-#         return Utils.SuccessResponse(data, "All users")
-#     except Exception as e:
-#         return Utils.ErrorResponse('Someting went wrong.')
-
-
-def scrapLater(exist):
-    Scrapper.scrapKeywords(exist)
-    print("Process Complete!!! for "+exist['_id']+" " + exist['targetType'])
-
-
-def my_scheduler():
-    targets = db.targets.find({})
-    for target in targets:
-        if target['status'] == 0:
-            target['_id'] = str(target['_id'])
-            scrapLater(target)
-
-
-@app.before_first_request
-def activate_scheduler():
-    scheduler = BackgroundScheduler(daemon=True)
-    scheduler.add_job(func=my_scheduler, trigger='interval', seconds=60)
-    scheduler.start()
-    print(" >>> Scheduler started")
-
-
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
--- utils/snsscrapper.py
+++ utils/snsscrapper.py
@@ -11,11 +11,11 @@ class Scrapper:
     def scrapKeywords(target):
         if (target['targetType'] == 'keywords'):
             if len(target['tweets']) == 0:
-                for keyword in eval(target['targets']):
+                for keyword in target['targets']:
                     tweets = []
                     snsScrapper = sntwitter.TwitterSearchScraper(keyword)
                     for counter, tweet in enumerate(snsScrapper.get_items()):
-                        if counter > target['limit']:
+                        if counter >= int(target['limit']):
                             break
                         tweets.append({'keyword': keyword, 'date': tweet.date, 'id': tweet.id, 'rawContent': tweet.rawContent, 'username': tweet.user.username})
                     if len(tweets) > 0:
@@ -27,11 +27,11 @@ class Scrapper:
                         print(">>> First Entry !!! No new tweets were found for "+exist['_id']+" " + exist['targetType'])
 
             else:
-                for keyword in eval(target['targets']):
+                for keyword in target['targets']:
                     tweets = []
                     snsScrapper = sntwitter.TwitterSearchScraper(keyword)
                     for counter, tweet in enumerate(snsScrapper.get_items()):
-                        if counter > target['limit']:
+                        if counter >= int(target['limit']):
                             break
                         exist = list(filter(lambda tweet: tweet["id"] == tweet['id'], target['tweets']))
                         if not exist:
@@ -47,70 +47,33 @@ class Scrapper:
                         print(">>> Scheduled!!! No new tweets were found for "+target['_id']+" " + target['targetType'] + " against keyword " + keyword)
 
         elif (target['targetType'] == 'twitter-hashtag'):
-            if len(target['tweets']) == 0:
-                for hashtag in eval(target['targets']):
-                    tweets = []
-                    snsScrapper = sntwitter.TwitterHashtagScraper(hashtag)
-                    for counter, tweet in enumerate(snsScrapper.get_items()):
-                        if counter > target['limit']:
-                            break
-                        tweets.append({'hashtag': hashtag, 'date': tweet.date, 'id': tweet.id, 'rawContent': tweet.rawContent, 'username': tweet.user.username})
-                    if len(tweets) > 0:
-                        update = {'$set': {'status': 1}, '$push': {'tweets': {'$each': tweets}}}
-                        result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
-                        print(f"First Entry !!! Matched {result.matched_count} documents for "+target['_id']+" " + target['targetType'] + " against hashtag " + hashtag)
-                        print(f"First Entry !!! Modified {result.modified_count} documents for "+target['_id']+" " + target['targetType'] + " against hashtag " + hashtag)
-                    else:
-                        print(">>> First Entry !!! No new tweets were found for "+target['_id']+" " + target['targetType'] + " against hashtag " + hashtag)
-
-            else:
-                for hashtag in eval(target['targets']):
-                    tweets = []
-                    snsScrapper = sntwitter.TwitterSearchScraper(hashtag)
-                    for counter, tweet in enumerate(snsScrapper.get_items()):
-                        if counter > target['limit']:
-                            break
-                        exist = list(filter(lambda tweet: tweet["id"] == tweet['id'], target['tweets']))
-                        if not exist:
-                            tweets.append({'hashtag': hashtag, 'date': tweet.date, 'id': tweet.id, 'rawContent': tweet.rawContent, 'username': tweet.user.username})
-                    if len(tweets) > 0:
-                        update = {'$set': {'status': 1}, '$push': {'tweets': {'$each': tweets}}}
-                        result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
-                        update = {'$set': {'status': 1}, '$push': {'tweets': {'$each': tweets}}}
-                        result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
-                        print(f"Scheduled : Matched {result.matched_count} documents.")
-                        print(f"Scheduled : Modified {result.modified_count} documents.")
-                    else:
-                        print(">>> Scheduled!!! No new tweets were found for "+target['_id']+" " + target['targetType'] + " against hashtag " + hashtag)
-
-        elif (target['targetType'] == 'twitter-user'):
             try:
                 if len(target['tweets']) == 0:
-                    for username in eval(target['targets']):
+                    for hashtag in target['targets']:
                         tweets = []
-                        snsScrapper = sntwitter.TwitterUserScraper(username)
+                        snsScrapper = sntwitter.TwitterSearchScraper('fire')
                         for counter, tweet in enumerate(snsScrapper.get_items()):
-                            if counter > target['limit']:
+                            if counter >= int(target['limit']):
                                 break
-                            tweets.append({'username': username, 'date': tweet.date, 'id': tweet.id, 'rawContent': tweet.rawContent, 'username': tweet.user.username})
+                            tweets.append({'hashtag': hashtag, 'date': tweet.date, 'id': tweet.id, 'rawContent': tweet.rawContent, 'username': tweet.user.username})
                         if len(tweets) > 0:
                             update = {'$set': {'status': 1}, '$push': {'tweets': {'$each': tweets}}}
                             result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
-                            print(f"First Entry !!! Matched {result.matched_count} documents for "+target['_id']+" " + target['targetType'] + " against username " + username)
-                            print(f"First Entry !!! Modified {result.modified_count} documents for "+target['_id']+" " + target['targetType'] + " against username " + username)
+                            print(f"First Entry !!! Matched {result.matched_count} documents for "+target['_id']+" " + target['targetType'] + " against hashtag " + hashtag)
+                            print(f"First Entry !!! Modified {result.modified_count} documents for "+target['_id']+" " + target['targetType'] + " against hashtag " + hashtag)
                         else:
-                            print(">>> First Entry !!! No new tweets were found for "+target['_id']+" " + target['targetType'] + " against username " + username)
+                            print(">>> First Entry !!! No new tweets were found for "+target['_id']+" " + target['targetType'] + " against hashtag " + hashtag)
 
                 else:
-                    for username in eval(target['targets']):
+                    for hashtag in target['targets']:
                         tweets = []
-                        snsScrapper = sntwitter.TwitterSearchScraper(username)
+                        snsScrapper = sntwitter.TwitterSearchScraper(hashtag)
                         for counter, tweet in enumerate(snsScrapper.get_items()):
-                            if counter > target['limit']:
+                            if counter >= int(target['limit']):
                                 break
                             exist = list(filter(lambda tweet: tweet["id"] == tweet['id'], target['tweets']))
                             if not exist:
-                                tweets.append({'username': username, 'date': tweet.date, 'id': tweet.id, 'rawContent': tweet.rawContent, 'username': tweet.user.username})
+                                tweets.append({'hashtag': hashtag, 'date': tweet.date, 'id': tweet.id, 'rawContent': tweet.rawContent, 'username': tweet.user.username})
                         if len(tweets) > 0:
                             update = {'$set': {'status': 1}, '$push': {'tweets': {'$each': tweets}}}
                             result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
@@ -119,9 +82,48 @@ class Scrapper:
                             print(f"Scheduled : Matched {result.matched_count} documents.")
                             print(f"Scheduled : Modified {result.modified_count} documents.")
                         else:
-                            print(">>> Scheduled!!! No new tweets were found for "+target['_id']+" " + target['targetType'] + " against username " + username)
+                            print(">>> Scheduled!!! No new tweets were found for "+target['_id']+" " + target['targetType'] + " against hashtag " + hashtag)
             except Exception as e:
                 print(e)
-                update = {'$set': {'status': 2}, }
 
-                result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
+        elif (target['targetType'] == 'twitter-user'):
+            # try:
+            if len(target['tweets']) == 0:
+                for username in target['targets']:
+                    tweets = []
+                    snsScrapper = sntwitter.TwitterUserScraper(username)
+                    for counter, tweet in enumerate(snsScrapper.get_items()):
+                        if counter >= int(target['limit']):
+                            break
+                        tweets.append({'username': username, 'date': tweet.date, 'id': tweet.id, 'rawContent': tweet.rawContent, 'username': tweet.user.username})
+                    if len(tweets) > 0:
+                        update = {'$set': {'status': 1}, '$push': {'tweets': {'$each': tweets}}}
+                        result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
+                        print(f"First Entry !!! Matched {result.matched_count} documents for "+target['_id']+" " + target['targetType'] + " against username " + username)
+                        print(f"First Entry !!! Modified {result.modified_count} documents for "+target['_id']+" " + target['targetType'] + " against username " + username)
+                    else:
+                        print(">>> First Entry !!! No new tweets were found for "+target['_id']+" " + target['targetType'] + " against username " + username)
+
+            else:
+                for username in target['targets']:
+                    tweets = []
+                    snsScrapper = sntwitter.TwitterUserScraper(username)
+                    for counter, tweet in enumerate(snsScrapper.get_items()):
+                        if counter >= int(target['limit']):
+                            break
+                        exist = list(filter(lambda tweet: tweet["id"] == tweet['id'], target['tweets']))
+                        if not exist:
+                            tweets.append({'username': username, 'date': tweet.date, 'id': tweet.id, 'rawContent': tweet.rawContent, 'username': tweet.user.username})
+                    if len(tweets) > 0:
+                        update = {'$set': {'status': 1}, '$push': {'tweets': {'$each': tweets}}}
+                        result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
+                        update = {'$set': {'status': 1}, '$push': {'tweets': {'$each': tweets}}}
+                        result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
+                        print(f"Scheduled : Matched {result.matched_count} documents.")
+                        print(f"Scheduled : Modified {result.modified_count} documents.")
+                    else:
+                        print(">>> Scheduled!!! No new tweets were found for "+target['_id']+" " + target['targetType'] + " against username " + username)
+            # except Exception as e:
+            #     print(e)
+            # update = {'$set': {'status': 2}, }
+            # result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
